6fddc297223d8c0638719c9306bae3606e6b68fc
sadithyakumar <adithyakumars2002@gmail.com>
2023-10-17 17:53:08 +0530

    Merge changes

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
-    unoptimized : true
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: 'export'
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  }
 }
 
 module.exports = nextConfig
